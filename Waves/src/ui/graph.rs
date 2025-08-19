c0e3cd2eceb4c56544987d968687d4e755011e7b
Zahr1729 <zahrgaming@gmail.com>
2025-08-19 18:14:29 +0100

    dots

--- Waves/src/ui/graph.rs
+++ Waves/src/ui/graph.rs
@@ -1,8 +1,32 @@
-use eframe::egui::{
-    self, Area, Color32, Frame, Id, Label, Margin, Pos2, Rect, Response, RichText, Sense, Stroke,
-    Ui, Vec2, Widget, Window, frame,
+use eframe::{
+    egui::{
+        self, Area, Color32, Context, DragValue, Frame, Grid, Id, Label, LayerId, Margin, Pos2,
+        Rect, Response, RichText, Sense, Shape, Slider, Stroke, StrokeKind, Ui, Vec2, Widget,
+        Window, frame,
+    },
+    epaint::{CircleShape, RectShape},
 };
 use egui_plot::Plot;
+
+use crate::main;
+
+pub struct NodeCircle {}
+
+impl Widget for NodeCircle {
+    fn ui(self, ui: &mut Ui) -> Response {
+        let frame = Frame::new()
+            .fill(Color32::BLUE)
+            .corner_radius(30.0)
+            .show(ui, |ui| ui.label(""));
+
+        //ui.allocate_rect(rect, sense)
+
+        if frame.response.hovered() {
+            //println!("hovered");
+        }
+        frame.response
+    }
+}
 
 #[derive(Clone)]
 pub struct Node {
@@ -24,6 +48,7 @@ impl Node {
         line_width: f32,
         header_height: f32,
         header_colour: Color32,
+        radius: f32,
     ) -> Response {
         let frame_pos = ui.next_widget_position();
 
@@ -31,7 +56,7 @@ impl Node {
             .inner_margin(margin)
             .corner_radius(corner_radius)
             .show(ui, |ui| {
-                let x = ui.available_width();
+                let x = ui.available_width() - radius;
                 let y = ui.available_height();
 
                 // This little function gets the position of our frame in screen space
@@ -45,7 +70,7 @@ impl Node {
                         },
                     frame_pos
                         + Vec2 {
-                            x: x + 2.0 * margin,
+                            x: x + margin * 2.0,
                             y: header_height,
                         },
                 );
@@ -55,7 +80,7 @@ impl Node {
                     frame_pos,
                     frame_pos
                         + Vec2 {
-                            x: x + 2.0 * margin,
+                            x: x + margin * 2.0,
                             y: header_height,
                         },
                 );
@@ -85,8 +110,8 @@ impl Node {
     }
 }
 
-impl Widget for &Node {
-    fn ui(self, ui: &mut Ui) -> Response {
+impl Widget for Node {
+    fn ui(mut self, ui: &mut Ui) -> Response {
         egui::Area::new(egui::Id::new(format!("graph_node {}", self.index)))
             .show(ui.ctx(), |ui| {
                 let line_width = 1.5;
@@ -96,6 +121,14 @@ impl Widget for &Node {
 
                 let header_colour = Color32::GRAY;
                 let main_colour = Color32::DARK_GRAY;
+                let circle_colour = Color32::BLUE;
+
+                let main_text_size = 16.0;
+                let grid_row_height = main_text_size + 0.0;
+
+                let radius = 6.0;
+
+                let pos = ui.next_widget_position();
 
                 egui::frame::Frame::new()
                     .stroke(Stroke::new(
@@ -114,6 +147,7 @@ impl Widget for &Node {
                             line_width,
                             header_height,
                             header_colour,
+                            radius,
                         );
 
                         // Main Content
@@ -121,32 +155,49 @@ impl Widget for &Node {
                         egui::frame::Frame::new()
                             .inner_margin(margin)
                             .show(ui, |ui| {
-                                ui.dnd_drag_source(
-                                    Id::new(format!("node_{}_output_0", self.index)),
-                                    "rose is stinky",
-                                    |ui| ui.label("This is an output"),
-                                );
+                                let grid = Grid::new("the_AWESOME_grid").show(ui, |ui| {
+                                    ui.add(Label::new(RichText::new("input").size(main_text_size)));
+                                    ui.add(Label::new(
+                                        RichText::new("output").size(main_text_size),
+                                    ));
+                                    ui.end_row();
 
-                                let (resp, dnd) = ui
-                                    .dnd_drop_zone::<&'static str, Response>(Frame::new(), |ui| {
-                                        ui.label("This is an input")
-                                    });
-
-                                let (resp, dnd) = ui
-                                    .dnd_drop_zone::<&'static str, Response>(Frame::new(), |ui| {
-                                        ui.label("This is an input")
-                                    });
-
-                                let (resp, dnd) = ui
-                                    .dnd_drop_zone::<&'static str, Response>(Frame::new(), |ui| {
-                                        ui.label("This is an input")
-                                    });
-
-                                if let Some(dnd) = dnd {
-                                    println!("Dropped '{dnd}' here!")
-                                }
+                                    ui.add(Label::new(RichText::new("input").size(main_text_size)));
+                                });
                             });
                     });
+
+                // Do the stuff with the selectible nodes
+                let radius = 6.0;
+
+                let offset = Vec2 {
+                    x: 0.0,
+                    y: grid_row_height * 1.25,
+                };
+
+                for i in 0..2 {
+                    // Want to get rect that is just offset from this so its on the edge
+                    let rect = Rect::from_two_pos(
+                        pos + Vec2 {
+                            x: -radius,
+                            y: header_height - radius + line_width + margin + grid_row_height * 0.7,
+                        } + offset * i as f32,
+                        pos + Vec2 {
+                            x: radius,
+                            y: header_height + radius + line_width + margin + grid_row_height * 0.7,
+                        } + offset * i as f32,
+                    );
+
+                    let circle = Shape::circle_filled(rect.center(), radius, circle_colour);
+                    ui.painter().add(circle);
+
+                    let r = ui.allocate_rect(rect, Sense::hover());
+
+                    //println!("{:?}", r);
+                    if r.hovered() {
+                        println!("UNGA BUNGA");
+                    }
+                }
             })
             .response
     }
@@ -169,19 +220,19 @@ impl NodeGraph {
                 Node {
                     index: 0,
                     pos: (100.0, 200.0),
-                    height: 200.0,
+                    height: 0.0,
                     width: 300.0,
                     inputs: vec![],
                     outputs: vec![],
                 },
-                Node {
-                    index: 1,
-                    pos: (20.0, 20.0),
-                    height: 200.0,
-                    width: 300.0,
-                    inputs: vec![],
-                    outputs: vec![],
-                },
+                // Node {
+                //     index: 1,
+                //     pos: (20.0, 20.0),
+                //     height: 200.0,
+                //     width: 300.0,
+                //     inputs: vec![],
+                //     outputs: vec![],
+                // },
             ],
             edges: vec![],
         }
@@ -195,7 +246,7 @@ impl Widget for NodeGraph {
         // area.with_widget_info(|| WidgetInfo::labeled(WidgetType::Window, true, title.text()));
         let mut r = None;
         for node in self.nodes {
-            let resp = ui.add(&node);
+            let resp = ui.add(node);
 
             r = Some(resp);
         }
